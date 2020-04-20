2227ce8efc42195be89e7a915120fc8ea707d7c4
Felix Fernandez <fernandezfelix460.gmail.com>
2020-04-20 15:53:14 -0400

    Updates

---
+++ precache-manifest.e2a313d4ec96e1a5ac715d6541281a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8556a258618493ce72233960de90fe",
+    "url": "/progler/index.html"
+  },
+  {
+    "revision": "f6780c04f30eacf37e81",
+    "url": "/progler/static/css/main.f864b929.chunk.css"
+  },
+  {
+    "revision": "81a896d84f9a43cac716",
+    "url": "/progler/static/js/2.3e702dee.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/progler/static/js/2.3e702dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6780c04f30eacf37e81",
+    "url": "/progler/static/js/main.bcde51b2.chunk.js"
+  },
+  {
+    "revision": "0688647404e7e63a3336",
+    "url": "/progler/static/js/runtime-main.d806db58.js"
+  }
+]);
